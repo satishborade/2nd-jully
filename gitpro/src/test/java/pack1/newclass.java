7c7bf7e2ff0ee772a38257459cc4bd617bbd8d87
dell <dell@DESKTOP-LRK2M9C>
2023-03-07 18:03:12 -0800

    First Commit

---
+++ gitpro/src/test/java/pack1/newclass.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class newclass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("TC01");
+		System.out.println("TC02");
+		System.out.println("TC03");
+		System.out.println("TC04");
+		
+		
+	}
+
+}
